d57ec6c3f14e9c49ffe4043e5be973bc4c3493aa
mhatiboglu <mh1741mh@gmail.com>
2020-03-10 14:37:41 +0200

    Use setState to handle data chnage

--- src/App.js
+++ src/App.js
@@ -11,12 +11,22 @@ class App extends Component {
     ]
   }
 
+  handleOnClick= ()=>{
+    //console.log('handled')
+    this.setState({
+      persons: [
+        { name: 'Muh', age: 20 },
+        { name: 'Betul', age: 6 },
+        { name: 'Merve', age: 28 }
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
         <p>Thi is really working</p>
-        <button>Switch Name</button>
+        <button onClick= {this.handleOnClick}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
