a45b8ca98e1acd519c19620515b6fbc3b06e08e2
mhatiboglu <mh1741mh@gmail.com>
2020-03-22 10:43:51 +0200

    Add onclick event to person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Person.css';
- 
- const Person = (props) => {
-     return  (
-     <div className='Person'> 
-         <p>I'm {props.name} and {props.age} years old.  </p>
-     <p style={{color:'red'}}>{props.children}</p>
-     <input type= 'text' onChange={props.changed} value={props.name}/>
-    </div> )
- }
 
- export default Person;
+const Person = (props) => {
+    return (
+        <div
+            className='Person'>
+            <p onClick={props.click}>
+                I'm {props.name} and {props.age} years old.  </p>
+            <p style={{ color: 'red' }}>{props.children}</p>
+            <input
+                type='text'
+                onChange={props.click}
+                value={props.name} />
+        </div>)
+}
+
+export default Person;
