a76c086afd0d0bbe5948baa9016cec316fabae47
mhatiboglu <mh1741mh@gmail.com>
2020-03-22 10:19:31 +0200

    Change preferred condition use.

--- src/App.js
+++ src/App.js
@@ -54,6 +54,17 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+    if (this.state.showPersons) {
+          persons = (<div >
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+              changed={this.nameChangedHandler} />
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> Hi there!</Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> Hi there!</Person>
+          </div>)
+    }
+
     return (
       <div className="App">
         <h1>Hi I'm a React App</h1>
@@ -61,13 +72,7 @@ class App extends Component {
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {this.state.showPersons ? <div >
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
-            changed={this.nameChangedHandler} />
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> Hi there!</Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> Hi there!</Person>
-        </div> : null}
+        {persons}
       </div>
     );
   }
