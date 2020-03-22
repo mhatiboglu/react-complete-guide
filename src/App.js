675b59b6ec40ef0304029b92e0e2809ef14bbcdd
mhatiboglu <mh1741mh@gmail.com>
2020-03-22 10:26:34 +0200

    Make dynamic to render persons from state

--- src/App.js
+++ src/App.js
@@ -57,11 +57,11 @@ class App extends Component {
     let persons = null;
     if (this.state.showPersons) {
           persons = (<div >
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
-              changed={this.nameChangedHandler} />
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> Hi there!</Person>
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> Hi there!</Person>
+            {this.state.persons.map(person =>{
+                return <Person 
+                  name={person.name}
+                  age= {person.age}/>
+           })}
           </div>)
     }
 
