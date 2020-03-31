939e986b7c222be3477dbf5249e4e089b5183607
mhatiboglu <mh1741mh@gmail.com>
2020-03-31 14:56:30 +0300

    Rebuild persons with persons component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../containers/App.css';
 import Radium from 'radium'
-import Person from '../components/Persons/Person/Person'
+import Persons from '../components/Persons/Persons'
 
 class App extends Component {
   state = {
@@ -65,16 +65,14 @@ class App extends Component {
     let persons = null;
 
     if (this.state.showPersons) {
-      persons = (<div >
-        {this.state.persons.map((person, index) => {
-          return <Person
-            click={() => this.deletePersonHandler(index)}
-            name={person.name}
-            age={person.age}
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)} />
-        })}
-      </div>)
+      persons = (
+        <div >
+          <Persons
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler} />
+        </div>
+      );
       style.backgroundColor = 'red';
       style[':hover'] = {
         backgroundColor: 'salmon',
